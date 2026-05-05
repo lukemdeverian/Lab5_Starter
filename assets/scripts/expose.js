72efd43867206a38cd0a02c8a6c13ad2ec082758
Luke Deverian <ldeverian@ucsd.edu>
2026-05-04 23:07:38 -0700

    add explose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,49 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('#expose img');
+  const audioElement = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('button');
+
+  const jsConfetti = new JSConfetti();
+
+  hornSelect.addEventListener('change', function () {
+    const selected = hornSelect.value;
+    hornImage.src = `assets/images/${selected}.png`;
+    hornImage.alt = selected;
+    audioElement.src = `assets/audio/${selected}.mp3`;
+  });
+
+  volumeSlider.addEventListener('input', function () {
+    const vol = parseInt(volumeSlider.value);
+    audioElement.volume = vol / 100;
+
+    if (vol === 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+      volumeIcon.alt = 'Volume level 0';
+    } else if (vol < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+      volumeIcon.alt = 'Volume level 1';
+    } else if (vol < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+      volumeIcon.alt = 'Volume level 2';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+      volumeIcon.alt = 'Volume level 3';
+    }
+  });
+
+  playButton.addEventListener('click', function () {
+    if (hornSelect.value === 'select') return;
+    audioElement.pause();
+    audioElement.currentTime = 0;
+    audioElement.play();
+
+    if (hornSelect.value === 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
 }
